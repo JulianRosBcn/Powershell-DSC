75583dc53400242499a40b06059044f45d19cf5d
Julian Ros <julianrosbcn@outlook.com>
2019-02-19 14:12:55 +0100

    Infra and APM Agent tested installation. Just APIKey needs to be replaced

---
+++ NewRelic/DSCConfigData.psd1
@@ -0,0 +1,30 @@
+#region HEADER
+# --------------------------------------------------------------------  
+# Desc     :  ConfigData to be processed during compilation
+# Contact  :  @julianrosbcn
+# Comments :  All nodes properties applies to all nodes detailed below
+@			  
+# --------------------------------------------------------------------
+#endregion HEADER
+ 
+$configdata = @{
+
+    AllNodes = @(
+		@{
+            NodeName = "*"
+        }
+		@{
+			NodeName = "prodServer"
+			Environment = "prod"
+		}
+		@{
+			NodeName = "devServer"
+			Environment = "dev"
+		}
+		@{
+			NodeName = "testServer"
+			Environment = "test"
+		}
+	)
+
+}
---
+++ NewRelic/DSCSettings.ps1
@@ -0,0 +1,74 @@
+#region HEADER
+# --------------------------------------------------------------------  
+# Desc     :  Install NR Agents APM and Infrastructure with DSC
+# Contact  :  @julianros
+# Comments :  Fully automated. Agents will start reporting after harvesting starts in some minutes after DSC is applies
+# --------------------------------------------------------------------
+#endregion HEADER
+
+
+
+
+Configuration DSCSettings
+{	
+	Import-DscResource -ModuleName  xPSDesiredStateConfiguration
+
+
+#region NewRelic Infrastructure Agent
+
+	 Node $AllNodes.where{$_.Environment -eq "prod"}.NodeName
+	 {
+
+		  $NRInfraAgentPackageLocalPath = "$($env:temp)\newrelic-infra.msi"
+		
+		 Service NRInfraAgent
+         {
+             Name = "newrelic-infra"
+             State = "Running"
+             DependsOn = "[Package]NRInfraAgent"
+         }
+
+         xRemoteFile NRInfraAgentPackage {
+             Uri = "https://download.newrelic.com/infrastructure_agent/windows/newrelic-infra.msi"
+             DestinationPath = $NRInfraAgentPackageLocalPath
+         }
+		 
+         Package NRInfraAgent {
+             Ensure = "present"
+             Path  = $NRInfraAgentPackageLocalPath
+             Name = "New Relic Infrastructure Agent"
+             ProductId = ""
+             Arguments = 'GENERATE_CONFIG="true" LICENSE_KEY="insert_your_apiKey_here"'
+             DependsOn = "[xRemoteFile]NRInfraAgentPackage"
+         }
+	
+	 }
+		
+#endregion
+ 
+#region NewRelic APM Agent
+	 
+	 Node $AllNodes.where{$_.Environment -eq "prod"}.NodeName
+	 {
+
+		  $NRDotNetAgentPackageLocalPath = "$($env:temp)\NewRelicDotNetAgent_x64.msi"
+
+         xRemoteFile NRDotNetAgentPackage {
+             Uri = "https://download.newrelic.com/dot_net_agent/latest_release/NewRelicDotNetAgent_x64.msi"
+             DestinationPath = $NRDotNetAgentPackageLocalPath
+         }
+		 
+         Package NRDotNetAgent {
+             Ensure = "Present"
+             Path  = $NRDotNetAgentPackageLocalPath
+             Name = "New Relic .NET Agent (64-bit)"
+             ProductId = ""
+             DependsOn = "[xRemoteFile]NRDotNetAgentPackage"
+         }
+	
+	 }
+
+#endregion
+
+
+}
